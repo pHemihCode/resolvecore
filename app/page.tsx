94abae9987ae03a15a2a2d07582c7339debe98c3
Femi <akintanfemi100@gmail.com>
2025-12-23 23:53:20 +0100

    Root page redirection fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/auth/sign-in");
+  }
+  redirect("/dashboard");
+  return <div>Redirecting...</div>;
 }
-
-export default page
